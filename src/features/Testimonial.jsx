a53d096251a37314f476c14028d05d482ea5013d
Graay Ukaegbu <graynneji405@gmail.com>
2024-01-23 21:57:59 -0800

    adding some icons

--- src/features/Testimonial.jsx
+++ src/features/Testimonial.jsx
@@ -104,8 +104,46 @@ function Testimonial() {
             </div>
           </div>
           <div>
-            <span>arrow</span>
-            <span>arrow</span>
+            <span>
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g id="arrow-left">
+                  <path
+                    id="Icon"
+                    d="M19 12H5M5 12L12 19M5 12L12 5"
+                    stroke="#175CD3"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </g>
+              </svg>
+            </span>
+            <span>
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g id="arrow-right">
+                  <path
+                    id="Icon"
+                    d="M5 12H19M19 12L12 5M19 12L12 19"
+                    stroke="#175CD3"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </g>
+              </svg>
+            </span>
           </div>
         </div>
       </div>
